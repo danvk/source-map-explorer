78bf0dc188c8f011d03641b10aaa3eb1d0c41207
Dan Vanderkam <danvdk@gmail.com>
2015-10-16 14:10:16 -0400

    Version bump

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ module.exports = {
 };
 
 if (require.main === module) {
-var args = docopt(doc, {version: '0.1'});
+var args = docopt(doc, {version: '1.1.0'});
 validateArgs(args);
 var data = loadSourceMap(args['<script.js>'], args['<script.js.map>']);
 if (!data) {
