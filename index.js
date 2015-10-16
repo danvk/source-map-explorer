9b8313241fc60aaa4c2f3687c9fd0db4f25eae51
Dan Vanderkam <danvdk@gmail.com>
2015-10-16 09:52:01 -0400

    Refactor and add command-line interface.

--- index.js
+++ index.js
@@ -1,51 +1,79 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony
+
+var doc = `
+Usage:
+  source-map-explorer [--json] <script.js> [<script.js.map>]
+
+  If the script file has an inline source map, you may omit the map parameter.
+
+Options:
+  --json     Output JSON (on stdout) instead of generating HTML
+             and opening the browser.
+  -h --help  Show this screen.
+  --version  Show version.
+`;
+
+
 var fs = require('fs'),
     path = require('path'),
     sourcemap = require('source-map'),
     convert = require('convert-source-map'),
     temp = require('temp'),
     open = require('open'),
-    _ = require('underscore');
+    _ = require('underscore'),
+    docopt = require('docopt').docopt;
 
-var sourcemapFile = process.argv[2],
-    jsFile = process.argv[3];
-
-var sourcemapData = fs.readFileSync(sourcemapFile).toString();
-var jsData = fs.readFileSync(process.argv[3]).toString();
-
-var sourcemap = new sourcemap.SourceMapConsumer(sourcemapData);
-sourcemap.computeColumnSpans();
-
-var lines = jsData.split('\n');
-var sourceExtrema = {};  // source -> {min: num, max: num}
-var numChars = 0;
-var lastSource = null;
-for (var line = 1; line <= lines.length; line++) {
-  var lineText = lines[line - 1];
-  var numCols = lineText.length;
-  for (var column = 0; column < numCols; column++, numChars++) {
-    var pos = sourcemap.originalPositionFor({line:line, column:column});
-    var source = pos.source;
-    if (source == null) {
-      // Often this is from the '// #sourceMap' comment itself.
-      continue;
-    }
-
-    if (source != lastSource) {
-      if (!(source in sourceExtrema)) {
-        sourceExtrema[source] = {min: numChars};
-        lastSource = source;
-      } else {
-        // source-map reports odd positions for bits between files.
+function computeGeneratedFileSizes(sourcemap, generatedJs) {
+  var lines = generatedJs.split('\n');
+  var sourceExtrema = {};  // source -> {min: num, max: num}
+  var numChars = 0;
+  var lastSource = null;
+  for (var line = 1; line <= lines.length; line++) {
+    var lineText = lines[line - 1];
+    var numCols = lineText.length;
+    for (var column = 0; column < numCols; column++, numChars++) {
+      var pos = sourcemap.originalPositionFor({line:line, column:column});
+      var source = pos.source;
+      if (source == null) {
+        // Often this is from the '// #sourceMap' comment itself.
+        continue;
       }
-    } else {
-      sourceExtrema[source].max = numChars;
+
+      if (source != lastSource) {
+        if (!(source in sourceExtrema)) {
+          sourceExtrema[source] = {min: numChars};
+          lastSource = source;
+        } else {
+          // source-map reports odd positions for bits between files.
+        }
+      } else {
+        sourceExtrema[source].max = numChars;
+      }
     }
   }
+  return _.mapObject(sourceExtrema, v => v.max - v.min + 1);
 }
-var sizes = _.mapObject(sourceExtrema, function(v) {
-  return v.max - v.min + 1;
-});
+
+function loadSourceMap(jsFile, mapFile) {
+  var sourcemapData = fs.readFileSync(mapFile).toString();
+  var jsData = fs.readFileSync(jsFile).toString();
+
+  var mapConsumer = new sourcemap.SourceMapConsumer(sourcemapData);
+
+  return {mapConsumer, jsData};
+}
+
+var args = docopt(doc, {version: '0.1'});
+var data = loadSourceMap(args['<script.js>'], args['<script.js.map>']),
+    mapConsumer = data.mapConsumer,
+    jsData = data.jsData;
+
+var sizes = computeGeneratedFileSizes(mapConsumer, jsData);
+
+if (args['--json']) {
+  console.log(JSON.stringify(sizes, null, '  '));
+  process.exit(0);
+}
 
 var html = fs.readFileSync(path.join(__dirname, 'tree-viz.html')).toString();
 
