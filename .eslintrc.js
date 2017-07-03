c92cf5491ccefaab184423b84a00ffeb73f2f463
Dan Vanderkam <danvdk@gmail.com>
2017-07-03 11:05:08 -0400

    Add eslint (#55)
    * Add eslint and fix issues
    
    * Add lint to test
    
    * bump node version on travis

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "mocha": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": 0
+    }
+};
