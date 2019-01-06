7356a9a975a3f6c7e38cace833a9b60f68120465
jinjing <jing.jin@zkh360.com>
2019-01-06 20:58:45 +0800

    fix windows eslint error.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,7 @@ module.exports = {
             "error",
             2
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
+        "linebreak-style": 0,
         "quotes": [
             "error",
             "single"
