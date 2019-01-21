d9827a4a659da9bff9db28cbd7816cc6739ac09a
Matt Jin <matt_jin@163.com>
2019-01-21 22:46:48 +0800

    not change linebreak-style

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,10 @@ module.exports = {
             "error",
             2
         ],
-        "linebreak-style": 0,
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
         "quotes": [
             "error",
             "single"
