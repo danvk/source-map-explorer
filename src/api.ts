cad00405c8c6b32c3766e094ee9268ae1c26bb26
nikolay-borzov <nikolay.n.borzov@gmail.com>
2019-04-07 11:33:43 +0400

    Update source-map dependency (#100)
    - Update `source-map` to `0.7.3`
    - Update dependencies
    - Add `chai-as-promised` to test async functions
    - Move types outside `types.d.ts` since TypeScript generates type definitions
    - Give variables more explicit names
    - Make `explore` async
    - Always set `bundleName` in the explore result
    - Create `ExploreErrorResult` during batch processing so that error can be reported later
    - Leverage snapshot testing for `explore` tests

--- src/api.ts
+++ src/api.ts
@@ -2,62 +2,80 @@ import path from 'path';
 import fs from 'fs';
 import fse from 'fs-extra';
 import os from 'os';
-import { SourceMapConsumer } from 'source-map';
+import { SourceMapConsumer, BasicSourceMapConsumer, IndexedSourceMapConsumer } from 'source-map';
 import convert from 'convert-source-map';
 
-import { reportUnmappedBytes, generateHtml, getBundles, UNMAPPED } from './common';
+import { reportUnmappedBytes, generateHtml, getBundles, UNMAPPED, Bundle } from './common';
 import { getFileContent, mapKeys, getCommonPathPrefix } from './helpers';
-
-type File = string | Buffer;
 
 const SOURCE_MAP_INFO_URL =
   'https://github.com/danvk/source-map-explorer/blob/master/README.md#generating-source-maps';
 
-/** Get source map */
-function loadSourceMap(jsFile: File, mapFile?: File) {
-  const jsData = getFileContent(jsFile);
-  let mapConsumer;
+const UNABLE_TO_FIND_SOURCE_MAP = `Unable to find a source map.${os.EOL}See ${SOURCE_MAP_INFO_URL}`;
 
-  if (mapFile) {
-    const sourcemapData = getFileContent(mapFile);
-    mapConsumer = new SourceMapConsumer(sourcemapData);
+type Consumer = BasicSourceMapConsumer | IndexedSourceMapConsumer;
+
+export type FileSizeMap = Record<string, number>;
+
+type File = string | Buffer;
+
+interface SourceMapData {
+  consumer: Consumer;
+  jsFileContent: string;
+}
+
+/** Get source map */
+async function loadSourceMap(jsFile: File, sourceMapFile?: File): Promise<SourceMapData> {
+  const jsFileContent = getFileContent(jsFile);
+
+  let consumer: Consumer;
+
+  if (sourceMapFile) {
+    const sourceMapFileContent = getFileContent(sourceMapFile);
+    consumer = await new SourceMapConsumer(sourceMapFileContent);
   } else {
     // Try to read a source map from a 'sourceMappingURL' comment.
-    let converter = convert.fromSource(jsData);
+    let converter = convert.fromSource(jsFileContent);
+
     if (!converter && !Buffer.isBuffer(jsFile)) {
-      converter = convert.fromMapFileSource(jsData, path.dirname(jsFile));
+      converter = convert.fromMapFileSource(jsFileContent, path.dirname(jsFile));
     }
+
     if (!converter) {
-      throw new Error(`Unable to find a source map.${os.EOL}See ${SOURCE_MAP_INFO_URL}`);
+      throw new Error(UNABLE_TO_FIND_SOURCE_MAP);
     }
-    mapConsumer = new SourceMapConsumer(converter.toJSON());
+
+    consumer = await new SourceMapConsumer(converter.toJSON());
   }
 
-  if (!mapConsumer) {
-    throw new Error(`Unable to find a source map.${os.EOL}See ${SOURCE_MAP_INFO_URL}`);
+  if (!consumer) {
+    throw new Error(UNABLE_TO_FIND_SOURCE_MAP);
   }
 
   return {
-    mapConsumer,
-    jsData,
+    consumer,
+    jsFileContent,
   };
 }
 
-function computeSpans(mapConsumer: any, generatedJs: string): Span[] {
-  const lines = generatedJs.split('\n');
+interface Span {
+  source: string | null;
+  numChars: number;
+}
+
+function computeSpans(mapConsumer: Consumer, jsFileContent: string): Span[] {
+  const lines = jsFileContent.split('\n');
   const spans: Span[] = [];
   let numChars = 0;
 
-  // TODO: Figure out why lastSource was set to false
-  let lastSource: string | null | undefined = undefined; // not a string, not null.
+  let lastSource: string | null | undefined = undefined; // not a string, not null
 
   for (let line = 1; line <= lines.length; line++) {
     const lineText = lines[line - 1];
     const numCols = lineText.length;
 
     for (let column = 0; column < numCols; column++, numChars++) {
-      const pos: { source: string | null } = mapConsumer.originalPositionFor({ line, column });
-      const source = pos.source;
+      const { source } = mapConsumer.originalPositionFor({ line, column });
 
       if (source !== lastSource) {
         lastSource = source;
@@ -71,15 +89,21 @@ function computeSpans(mapConsumer: any, generatedJs: string): Span[] {
   return spans;
 }
 
+interface FileSizes {
+  files: FileSizeMap;
+  unmappedBytes: number;
+  totalBytes: number;
+}
+
 /** Calculate the number of bytes contributed by each source file */
-function computeGeneratedFileSizes(mapConsumer: any, generatedJs: string) {
-  const spans = computeSpans(mapConsumer, generatedJs);
+function computeGeneratedFileSizes(consumer: Consumer, jsFileContent: string): FileSizes {
+  const spans = computeSpans(consumer, jsFileContent);
 
-  let unmappedBytes = 0;
   const files: Record<string, number> = {};
-  var totalBytes = 0;
+  let unmappedBytes = 0;
+  let totalBytes = 0;
 
-  for (var i = 0; i < spans.length; i++) {
+  for (let i = 0; i < spans.length; i++) {
     const { numChars, source } = spans[i];
 
     totalBytes += numChars;
@@ -106,10 +130,10 @@ export function adjustSourcePaths(
 ): FileSizeMap {
   if (findRoot) {
     const prefix = getCommonPathPrefix(Object.keys(sizes));
-    const len = prefix.length;
+    const length = prefix.length;
 
-    if (len) {
-      sizes = mapKeys(sizes, source => source.slice(len));
+    if (length) {
+      sizes = mapKeys(sizes, source => source.slice(length));
     }
   }
 
@@ -119,7 +143,7 @@ export function adjustSourcePaths(
 
   var finds = Object.keys(replace);
 
-  for (var i = 0; i < finds.length; i++) {
+  for (let i = 0; i < finds.length; i++) {
     const before = new RegExp(finds[i]);
     const after = replace[finds[i]];
 
@@ -129,29 +153,47 @@ export function adjustSourcePaths(
   return sizes;
 }
 
+export interface ExploreOptions {
+  onlyMapped?: boolean;
+  html?: boolean;
+  noRoot?: boolean;
+  replace?: ReplaceMap;
+}
+
+type ReplaceMap = Record<string, string>;
+
+export interface ExploreResult {
+  bundleName: string;
+  totalBytes: number;
+  unmappedBytes?: number;
+  files: FileSizeMap;
+  html?: string;
+}
+
+interface ExploreErrorResult {
+  bundleName: string;
+  error: NodeJS.ErrnoException;
+}
+
 /** Analyze bundle */
-export function explore(code: File, map?: File, options?: ExploreOptions): ExploreResult {
-  if (typeof options === 'undefined') {
-    if (typeof map === 'object' && !Buffer.isBuffer(map)) {
-      options = map;
-      map = undefined;
-    }
+export async function explore(
+  code: File,
+  map?: File,
+  options?: ExploreOptions
+): Promise<ExploreResult> {
+  if (typeof options === 'undefined' && typeof map === 'object' && !Buffer.isBuffer(map)) {
+    options = map;
+    map = undefined;
   }
 
   if (!options) {
     options = {};
   }
 
-  const data = loadSourceMap(code, map);
+  const { jsFileContent, consumer } = await loadSourceMap(code, map);
 
-  // TODO: Remove. `data` is always set
-  if (!data) {
-    throw new Error('Failed to load script and sourcemap');
-  }
+  const sizes = computeGeneratedFileSizes(consumer, jsFileContent);
 
-  const { mapConsumer, jsData } = data;
-
-  const sizes = computeGeneratedFileSizes(mapConsumer, jsData);
   let files = sizes.files;
 
   const filenames = Object.keys(files);
@@ -173,38 +215,23 @@ export function explore(code: File, map?: File, options?: ExploreOptions): Explo
     files[UNMAPPED] = unmappedBytes;
   }
 
+  const bundleName = Buffer.isBuffer(code) ? 'Buffer' : code;
+
   const result: ExploreResult = {
-    bundleName: 'bundle', // TODO: Temporary to merge ExploreResult and ExploreBatchResult
+    bundleName,
     totalBytes,
     unmappedBytes,
     files,
   };
 
   if (options.html) {
-    const title = Buffer.isBuffer(code) ? 'Buffer' : code;
-    result.html = generateHtml([
-      {
-        ...result,
-        bundleName: title,
-      },
-    ]);
+    result.html = generateHtml([result]);
   }
 
+  // Free Wasm data
+  consumer.destroy();
+
   return result;
-}
-
-/**
- * Wrap `explore` with Promise
- */
-function explorePromisified({ codePath, mapPath }: Bundle): Promise<ExploreResult> {
-  return new Promise<ExploreResult>(resolve => {
-    const result = explore(codePath, mapPath);
-
-    resolve({
-      ...result,
-      bundleName: codePath,
-    });
-  });
 }
 
 /**
@@ -223,37 +250,54 @@ function onExploreError(bundleInfo: Bundle, err: NodeJS.ErrnoException): void {
  *
  * @param bundles Bundles to explore
  */
-export function exploreBundlesAndFilterErroneous(
+export async function exploreBundles(
   bundles: Bundle[]
-): Promise<(void | ExploreResult)[]> {
+): Promise<(ExploreResult | ExploreErrorResult)[]> {
   return Promise.all(
-    bundles.map(bundle => explorePromisified(bundle).catch(err => onExploreError(bundle, err)))
-  ).then(results => results.filter(data => data));
+    bundles.map(bundle =>
+      explore(bundle.codePath, bundle.mapPath).catch<ExploreErrorResult>(error => {
+        onExploreError(bundle, error);
+
+        return {
+          bundleName: bundle.codePath,
+          error,
+        };
+      })
+    )
+  );
 }
 
+interface WriteConfig {
+  path?: string;
+  fileName: string;
+}
+
+// TODO: Merge into `explore`
 /**
  * Explore multiple bundles and write html output to file.
  *
  * @param codePath Path to bundle file or glob matching bundle files
  * @param [mapPath] Path to bundle map file
  */
-export function exploreBundlesAndWriteHtml(
+export async function exploreBundlesAndWriteHtml(
   writeConfig: WriteConfig,
   codePath: string,
   mapPath?: string
 ): Promise<void> {
   const bundles = getBundles(codePath, mapPath);
 
-  return exploreBundlesAndFilterErroneous(bundles).then(results => {
-    if (results.length === 0) {
+  return exploreBundles(bundles).then(results => {
+    const successResults = results.filter(
+      (result): result is ExploreResult => result.hasOwnProperty('files')
+    );
+
+    if (successResults.length === 0) {
       throw new Error('There were errors');
     }
 
-    // @ts-ignore TODO: Promise.all returns (void | ExploreResult)[] find a way to filter out void
-    results.forEach(reportUnmappedBytes);
+    successResults.forEach(reportUnmappedBytes);
 
-    // @ts-ignore TODO: Promise.all returns (void | ExploreResult)[] find a way to filter out void
-    const html = generateHtml(results);
+    const html = generateHtml(successResults);
 
     if (writeConfig.path !== undefined) {
       // Use fse to support older node versions
