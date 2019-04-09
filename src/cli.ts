b036e59d01bef827adce1879ac89930087bc9724
Nikolay Borzov <nikolay.n.borzov@gmail.com>
2019-04-10 00:29:11 +0400

    Replace docopt by yargs (#108)
    - Replace `--noroot` by `--no-root`
    - Update dependencies

--- src/cli.ts
+++ src/cli.ts
@@ -1,100 +1,127 @@
 #!/usr/bin/env node
 
-import { docopt } from 'docopt';
+import yargs from 'yargs';
 import fs from 'fs';
 import temp from 'temp';
 import open from 'open';
 
-// TODO: https://github.com/Microsoft/TypeScript/issues/24744
-// import packageJson from '../package.json';
-
-import { explore, exploreBundles, ExploreOptions, ExploreResult } from './api';
+import { explore, exploreBundles, ExploreOptions, ExploreResult, ReplaceMap } from './api';
 import { reportUnmappedBytes, generateHtml, getBundles } from './common';
 
-const doc = [
-  'Analyze and debug space usage through source maps.',
-  '',
-  'Usage:',
-  '  source-map-explorer <script.js> [<script.js.map>]',
-  '  source-map-explorer [--json | --html | --tsv] [-m | --only-mapped] <script.js> [<script.js.map>] [--replace=BEFORE --with=AFTER]... [--noroot]',
-  '  source-map-explorer -h | --help | --version',
-  '',
-  'If the script file has an inline source map, you may omit the map parameter.',
-  '',
-  'Options:',
-  '  -h --help  Show this screen.',
-  '  --version  Show version.',
-  '',
-  '  --json  Output JSON (on stdout) instead of generating HTML',
-  '          and opening the browser.',
-  '  --tsv   Output TSV (on stdout) instead of generating HTML',
-  '          and opening the browser.',
-  '  --html  Output HTML (on stdout) rather than opening a browser.',
-  '',
-  '  -m --only-mapped  Exclude "unmapped" bytes from the output.',
-  '                    This will result in total counts less than the file size',
-  '',
-  '',
-  '  --noroot  To simplify the visualization, source-map-explorer',
-  '            will remove any prefix shared by all sources. If you',
-  '            wish to disable this behavior, set --noroot.',
-  '',
-  '  --replace=BEFORE  Apply a simple find/replace on source file',
-  '                    names. This can be used to fix some oddities',
-  '                    with paths which appear in the source map',
-  '                    generation process.  Accepts regular expressions.',
-  '  --with=AFTER  See --replace.',
-].join('\n');
-
-interface Args {
-  /** Path to code file or Glob matching bundle files */
-  '<script.js>': string;
-  /** Path to map file */
-  '<script.js.map>'?: string;
-  '--json'?: boolean;
-  '--html'?: boolean;
-  '--tsv'?: boolean;
-  '--only-mapped'?: boolean;
-  '-m': boolean;
-  '--replace'?: string[];
-  '--with'?: string[];
-  '--noroot'?: boolean;
+interface Arguments {
+  _: string[];
+  json?: boolean;
+  tsv?: boolean;
+  html?: boolean;
+  onlyMapped?: boolean;
+  noRoot?: boolean;
+  replace?: string[];
+  with?: string[];
 }
 
-/**
- * Validate CLI arguments
- */
-function validateArgs(args: Args): void {
-  if (args['--replace'] && args['--with'] && args['--replace'].length !== args['--with'].length) {
-    console.error('--replace flags must be paired with --with flags.');
-    process.exit(1);
-  }
+function parseArguments(): Arguments {
+  // TODO: Use `middleware` to remove extra single quotes from positional arguments
+  return yargs
+    .strict()
+    .scriptName('source-map-explorer')
+    .usage('Analyze and debug space usage through source maps.')
+    .usage('Usage:')
+    .usage(
+      '$0 script.js [script.js.map] [--json | --html | --tsv] [-m | --only-mapped] [--replace=BEFORE_1 BEFORE_2 --with=AFTER_1 AFTER_2] [--no-root] [--version] [--help | -h]'
+    )
+    .example('$0 script.js script.js.map', 'Explore bundle')
+    .example('$0 script.js', 'Explore bundle with inline source map')
+    .example('$0 dist/js/*.*', 'Explore all bundles inside dist/js folder')
+    .demandCommand(1, 'At least one js file must be specified')
+    .options({
+      json: {
+        type: 'boolean',
+        description: 'Output JSON (on stdout) instead of generating HTML and opening the browser.',
+        conflicts: ['tsv', 'html'],
+      },
+      tsv: {
+        type: 'boolean',
+        description: 'Output TSV (on stdout) instead of generating HTML and opening the browser.',
+        conflicts: ['json', 'html'],
+      },
+      html: {
+        type: 'boolean',
+        description: 'Output HTML (on stdout) rather than opening a browser.',
+        conflicts: ['json', 'tsv'],
+      },
+
+      'only-mapped': {
+        alias: 'm',
+        type: 'boolean',
+        description:
+          'Exclude "unmapped" bytes from the output. This will result in total counts less than the file size',
+      },
+
+      'no-root': {
+        type: 'boolean',
+        description:
+          'To simplify the visualization, source-map-explorer will remove any prefix shared by all sources. If you wish to disable this behavior, set --no-root.',
+      },
+
+      replace: {
+        type: 'string',
+        array: true,
+        description:
+          'Apply a simple find/replace on source file names. This can be used to fix some oddities with paths which appear in the source map  generation process. Accepts regular expressions.',
+        implies: 'with',
+      },
+      with: {
+        type: 'string',
+        array: true,
+        description: 'See --replace.',
+        implies: 'replace',
+      },
+    })
+    .group(['json', 'tsv', 'html'], 'Output:')
+    .group(['replace', 'with'], 'Replace:')
+    .help('h')
+    .alias('h', 'help')
+    .showHelpOnFail(false, 'Specify --help for available options')
+    .wrap(null) // Do not limit line length
+    .parserConfiguration({
+      'boolean-negation': false,
+    })
+    .check(argv => {
+      if (argv.replace && argv.with && argv.replace.length !== argv.with.length) {
+        throw new Error('--replace flags must be paired with --with flags');
+      }
+
+      return true;
+    })
+    .parse();
 }
 
 /**
  * Create options object for `explore` method
- * @param  args CLI arguments
+ * @param  argv CLI arguments
  */
-function getExploreOptions(args: Args): ExploreOptions {
+function getExploreOptions(argv: Arguments): ExploreOptions {
   let html = true;
-  if (args['--json'] || args['--tsv']) {
+  if (argv.json || argv.tsv) {
     html = false;
   }
 
-  const replace: Record<string, string> = {};
-  const argsReplace = args['--replace'];
-  const argsWith = args['--with'];
+  const replaceItems = argv.replace;
+  const replaceWithItems = argv.with;
 
-  if (argsReplace && argsWith) {
-    for (let replaceIndex = 0; replaceIndex < argsReplace.length; replaceIndex += 1) {
-      replace[argsReplace[replaceIndex]] = argsWith[replaceIndex];
-    }
-  }
+  const replace =
+    replaceItems && replaceWithItems
+      ? replaceItems.reduce<ReplaceMap>((result, item, index) => {
+          result[item] = replaceWithItems[index];
+
+          return result;
+        }, {})
+      : undefined;
 
   return {
-    onlyMapped: args['--only-mapped'] || args['-m'],
+    onlyMapped: argv.onlyMapped,
     html,
-    noRoot: !!args['--noroot'],
+    noRoot: argv.noRoot,
     replace,
   };
 }
@@ -121,17 +148,15 @@ function writeToHtml(html?: string): void {
 }
 
 if (require.main === module) {
-  const args: Args = docopt(doc, { version: '2.0.0' /* packageJson.version*/ });
+  const argv = parseArguments();
 
-  validateArgs(args);
-
-  const bundles = getBundles(args['<script.js>'], args['<script.js.map>']);
+  const bundles = getBundles(argv._[0], argv._[1]);
 
   if (bundles.length === 0) {
     throw new Error('No file(s) found');
   }
 
-  const exploreOptions = getExploreOptions(args);
+  const exploreOptions = getExploreOptions(argv);
 
   if (bundles.length === 1) {
     const { codePath, mapPath } = bundles[0];
@@ -140,17 +165,17 @@ if (require.main === module) {
       .then(result => {
         reportUnmappedBytes(result);
 
-        if (args['--json']) {
+        if (argv.json) {
           console.log(JSON.stringify(result.files, null, '  '));
           process.exit(0);
-        } else if (args['--tsv']) {
+        } else if (argv.tsv) {
           console.log('Source\tSize');
           Object.keys(result.files).forEach(source => {
             const size = result.files[source];
             console.log(`${size}\t${source}`);
           });
           process.exit(0);
-        } else if (args['--html']) {
+        } else if (argv.html) {
           console.log(result.html);
           process.exit(0);
         }
@@ -181,7 +206,7 @@ if (require.main === module) {
       const html = generateHtml(successResults);
 
       // Check args instead of exploreOptions.html because it always true
-      if (args['--html']) {
+      if (argv.html) {
         console.log(html);
       } else {
         writeToHtml(html);
