f2c1b61fc98f0c061a1581741cebabdf3224a988
nikolay-borzov <nikolay.n.borzov@gmail.com>
2020-02-22 17:43:18 +0400

    Add performance test (#166)
    - Use `PerformanceObserver` to get duration to make actual execution time visible in failed test

--- tests/performance/api.test.ts
+++ tests/performance/api.test.ts
@@ -1,3 +1,6 @@
+import { expect } from 'chai';
+import { PerformanceObserver, performance } from 'perf_hooks';
+
 import { explore } from '../../src/api';
 import { setTestFolder } from '../test-helpers';
 
@@ -7,10 +10,22 @@ describe('api', () => {
   describe('explore', () => {
     setTestFolder();
 
-    it(`should explore big file in less than ${BIG_FILE_EXPLORE}ms`, async function() {
-      this.timeout(BIG_FILE_EXPLORE);
+    it(`should explore big file in less than ${BIG_FILE_EXPLORE}ms`, async () => {
+      const obs = new PerformanceObserver(items => {
+        const duration = items.getEntries()[0].duration;
 
+        expect(duration).to.be.lessThan(BIG_FILE_EXPLORE);
+
+        performance.clearMarks();
+        obs.disconnect();
+      });
+
+      obs.observe({ entryTypes: ['measure'] });
+
+      performance.mark('A');
       await explore('data/big.js');
+      performance.mark('B');
+      performance.measure('A to B', 'A', 'B');
     });
   });
 });
