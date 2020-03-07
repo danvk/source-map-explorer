1a9c5ca459e6997be50983422d6fb28b63698d7d
nikolay-borzov <nikolay.n.borzov@gmail.com>
2020-03-07 22:34:30 +0400

    Merge generate-data into main project (#141)
    - Adjust perf test threshold

--- tests/performance/api.test.ts
+++ tests/performance/api.test.ts
@@ -5,7 +5,7 @@ import { explore } from '../../src/api';
 import { setTestFolder } from '../test-helpers';
 
 // Set to value actual for CI (not your local environment)
-const BIG_FILE_EXPLORE_WITH_HTML_MS = 7000;
+const BIG_FILE_EXPLORE_WITH_HTML_MS = 8000;
 
 describe('api', () => {
   describe('explore', () => {
